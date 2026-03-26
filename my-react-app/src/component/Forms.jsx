acdd07182ecc6eac00706f0f315379e9adf50832
Nikilpn <nikhilpnnick1234@gmail.com>
2026-03-26 14:33:52 +0530

    beginner levle react learining input fields events

--- my-react-app/src/component/Forms.jsx
+++ my-react-app/src/component/Forms.jsx
@@ -2,16 +2,35 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Forms extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
+    handleChange=(event)=>{
+        // console.log(event.target.value);
+        this.setState(
+            {
+                [event.target.name]:event.target.value
+            }
+        )
+    }
+
+
+    Onsubmit=()=>{
+        console.log(this.state)
+
+    }
+
+
+    state={
+        firstname:'',
+        lastname:''
+    };
 
   render() {
     return (
       <div>
         <form action="">
             <h1>form component</h1>
-            <input type="text" />
+            <input type="text" name='firstname' value={this.state.firstname} onChange={this.handleChange}/>
+            <input type="text" name='lastname'  value={this.state.lastname} onChange={this.handleChange}/>
+            <button type='button' onClick={this.Onsubmit}>buttons</button>
         </form>
         
       </div>
