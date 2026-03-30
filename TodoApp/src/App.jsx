b907a7216bea6465118206bbfa61fe1e2c39859c
Nikilpn <nikhilpnnick1234@gmail.com>
2026-03-30 10:45:02 +0530

    beginner level react implementing router

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
-import Todoapp from './components/TodoApp/Todoapp'
-
+import Todoapp from "./components/Todoapp/Todoapp";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default class App extends Component {
-
-
   render() {
     return (
-      <div>
-          <Todoapp />
-        
-      </div>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" exact element={<Todoapp />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+      </Router>
     )
   }
 }
---
+++ TodoApp/src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1 style={{color:'white',textAlign:'center'}}>About component</h1>
+      
+    </div>
+  )
+}
