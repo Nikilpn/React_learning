196a3907b543cee43b577c2e4af236271b177915
Nikilpn <nikhilpnnick1234@gmail.com>
2026-04-25 11:56:14 +0530

    react lectures introduction and components and styling

---
+++ demo/src/App.jsx
@@ -0,0 +1,24 @@
+import Demo from "./Demo"
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <h1 style={{color:"red",fontSize:"50px"}}>welcome</h1>
+     <p id ="result">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate incidunt mollitia
+       esse quidem reiciendis, harum dolorum
+       optio alias facere veniam, autem est totam impedit quo doloribus
+        assumenda distinctio ex consequuntur!</p>
+      </div>
+
+      <Demo/>
+
+    </>
+  )
+}
+
+export default App
