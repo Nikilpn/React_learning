4c9a96cfc93fbc3bdb5de2a744027f8decddd423
Nikilpn <nikhilpnnick1234@gmail.com>
2026-05-21 17:32:47 +0530

    react learning curve

--- EduTechwebsite/techflix-website/src/App.jsx
+++ EduTechwebsite/techflix-website/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 import './App.css'
-import Task1 from './components/Task/task1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Task1 />
-
+      hello
     </>
   )
 }
